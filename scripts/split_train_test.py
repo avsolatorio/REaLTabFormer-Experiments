62962b6cf429e4fff241f00ab012dca06ee4bbcb
Aivin V. Solatorio <avsolatorio@gmail.com>
2022-12-20 19:46:44 -0500

    Remove unused imports
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- scripts/split_train_test.py
+++ scripts/split_train_test.py
@@ -1,7 +1,5 @@
 from typing import Dict, Optional
-import random
 import pandas as pd
-import numpy as np
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 import joblib
