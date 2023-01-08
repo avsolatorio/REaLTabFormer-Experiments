a275003f5185ee330e745af95ab1457a8c85873c
Aivin V. Solatorio <avsolatorio@gmail.com>
2023-01-07 21:18:57 -0500

    Update configs and update realtabformer
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

---
+++ scripts/gen_updated_realtabformer_config.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import shutil
+from config_lib import load_config
+
+
+EXP_DIR = Path(__file__).parent.parent / "exp"
+print(EXP_DIR)
+
+src_config = EXP_DIR / "california" / "realtabformer" / "config.toml"
+
+conf = src_config.read_text()
+
+for data_path in EXP_DIR.glob("*"):
+    if data_path.is_file() or data_path.name == "california":
+        continue
+
+    rtf_dir = (data_path / "realtabformer")
+
+    rtf_dir.mkdir(parents=True, exist_ok=True)
+
+    # if (rtf_dir / "config.toml").exists():
+    #     continue
+    # else:
+
+    cconf = str(conf)
+    cconf = cconf.replace("/california/", f"/{data_path.name}/")
+    (rtf_dir / "config.toml").write_text(cconf)
+
+    # shutil.copy2(src_config, rtf_dir / "config.toml")
+    assert (rtf_dir / "config.toml").exists()
