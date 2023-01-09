d594b0491a7ceeafda49612975b8fe41b1000b9c
Aivin V. Solatorio <avsolatorio@gmail.com>
2023-01-08 22:55:58 -0500

    Update config generation and configs to use target_col
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- scripts/gen_updated_realtabformer_config.py
+++ scripts/gen_updated_realtabformer_config.py
@@ -6,12 +6,12 @@ from config_lib import load_config
 EXP_DIR = Path(__file__).parent.parent / "exp"
 print(EXP_DIR)
 
-src_config = EXP_DIR / "california" / "realtabformer" / "config.toml"
+src_config = EXP_DIR / "base_rtf_config.toml"
 
 conf = src_config.read_text()
 
 for data_path in EXP_DIR.glob("*"):
-    if data_path.is_file() or data_path.name == "california":
+    if data_path.is_file():
         continue
 
     rtf_dir = (data_path / "realtabformer")
@@ -23,7 +23,7 @@ for data_path in EXP_DIR.glob("*"):
     # else:
 
     cconf = str(conf)
-    cconf = cconf.replace("/california/", f"/{data_path.name}/")
+    cconf = cconf.replace("/DATA_ID/", f"/{data_path.name}/")
     (rtf_dir / "config.toml").write_text(cconf)
 
     # shutil.copy2(src_config, rtf_dir / "config.toml")
