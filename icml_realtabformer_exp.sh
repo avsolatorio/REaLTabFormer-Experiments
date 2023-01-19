6e48f0b871811469c6758fb48beeb2e578fb1679
Aivin V. Solatorio <avsolatorio@gmail.com>
2023-01-19 15:19:57 -0500

    Parameterize script
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

---
+++ icml_realtabformer_exp.sh
@@ -0,0 +1,19 @@
+PYTHON_RTF_ENV=$1
+FROM_EXP_VERSION=$2
+CUDA_DEVICE=$3
+
+
+PYTHON_RTF_ENV scripts/run_experiments.py --run_data_id --data_id=diabetes --cuda_device=${CUDA_DEVICE} --from_exp_version=${FROM_EXP_VERSION}
+
+PYTHON_RTF_ENV scripts/run_experiments.py --run_data_id --data_id=california --cuda_device=${CUDA_DEVICE} --from_exp_version=${FROM_EXP_VERSION}
+
+PYTHON_RTF_ENV scripts/run_experiments.py --run_data_id --data_id=adult --cuda_device=${CUDA_DEVICE} --from_exp_version=${FROM_EXP_VERSION}
+
+PYTHON_RTF_ENV scripts/run_experiments.py --run_data_id --data_id=fb-comments --cuda_device=${CUDA_DEVICE} --from_exp_version=${FROM_EXP_VERSION}
+
+
+PYTHON_RTF_ENV scripts/run_experiments.py --run_data_id --data_id=abalone --cuda_device=${CUDA_DEVICE} --from_exp_version=${FROM_EXP_VERSION}
+
+PYTHON_RTF_ENV scripts/run_experiments.py --run_data_id --data_id=cardio --cuda_device=${CUDA_DEVICE} --from_exp_version=${FROM_EXP_VERSION}
+
+PYTHON_RTF_ENV scripts/run_experiments.py --run_data_id --data_id=buddy --cuda_device=${CUDA_DEVICE} --from_exp_version=${FROM_EXP_VERSION}
